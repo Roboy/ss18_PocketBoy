1bd11b9b99842784a9eac2ec79282be36b73c4b9
soapinduty <g4mrator@googlemail.com>
2018-08-13 16:55:53 +0200

    Arrow points now in 3D towards roboy, loading animation for spheres added.

--- Assets/Scripts/AR/PositionIndicator.cs
+++ Assets/Scripts/AR/PositionIndicator.cs
@@ -9,10 +9,8 @@
 
     public class PositionIndicator : MonoBehaviour
     {
-        public Text t;
-        public Image arrowLeft;
-        public Image arrowRight;
         public Camera cam;
+        public GameObject indicator;
 
         private GameObject m_roboyModel;
         private bool m_indicating;
@@ -22,18 +20,20 @@
         private void Update()
         {
 
-
+            //Get a reference to the roboy model
             if (m_roboyModel == null && m_roboyFound == false)
             {
-                t.text = ("no roboy yet");
+                Debug.Log("No Roboy yet.");
                 m_roboyModel = GameObject.FindGameObjectWithTag("Roboy");
                 if (m_roboyModel != null)
                 {
                     m_roboyFound = true;
-                    Debug.Log("Roboy found.");
+                    Debug.Log("There he is!");
+                    
                 }
             }
 
+            //Get a reference to all of the renderers that roboy has
             if (m_renderers == null && m_roboyFound == true)
             {
                 m_renderers = m_roboyModel.GetComponentsInChildren<Renderer>();
@@ -46,8 +46,7 @@
                     if (r.isVisible)
                     {
                         m_indicating = false;
-                        arrowLeft.gameObject.SetActive(false);
-                        arrowRight.gameObject.SetActive(false);
+                        indicator.gameObject.SetActive(false);
                         break;
                     }
 
@@ -55,29 +54,33 @@
                 }
             }
 
+            //In case that the roboy model is not visible, show the position via arrows
             if (m_roboyFound && m_indicating)
             {
-                //Direction in which the camera is looking
-                Vector2 cameraForward = new Vector2(cam.transform.forward.x, cam.transform.forward.z);
-                cameraForward.Normalize();
-                //Direction from camera to roboy
-                Vector2 towardsRoboy = new Vector2(m_roboyModel.transform.position.x - cam.transform.position.x, m_roboyModel.transform.position.z - cam.transform.position.z);
-                towardsRoboy.Normalize();
+                ////Direction in which the camera is looking
+                //Vector2 cameraForward = new Vector2(cam.transform.forward.x, cam.transform.forward.z);
+                //cameraForward.Normalize();
+                ////Direction from camera to roboy
+                //Vector2 towardsRoboy = new Vector2(m_roboyModel.transform.position.x - cam.transform.position.x, m_roboyModel.transform.position.z - cam.transform.position.z);
+                //towardsRoboy.Normalize();
 
-                float result = cameraForward.x - towardsRoboy.x;
-                if (result > 0)
-                {
-                    arrowLeft.gameObject.SetActive(true);
-                    arrowRight.gameObject.SetActive(false);
-                }
-                if (result < 0)
-                {
-                    arrowLeft.gameObject.SetActive(false);
-                    arrowRight.gameObject.SetActive(true);
-                }
+                //float result = cameraForward.x - towardsRoboy.x;
+                //if (result > 0)
+                //{
+                //    arrowLeft.gameObject.SetActive(true);
+                //    arrowRight.gameObject.SetActive(false);
+                //}
+                //if (result < 0)
+                //{
+                //    arrowLeft.gameObject.SetActive(false);
+                //    arrowRight.gameObject.SetActive(true);
+                //}
+
+                indicator.gameObject.SetActive(true);
+                Vector3 tmp = new Vector3(m_roboyModel.transform.position.x, m_roboyModel.transform.position.y + 0.5f, m_roboyModel.transform.position.z);
+                indicator.transform.LookAt(tmp);
             }
 
-            
 
         }
     }
