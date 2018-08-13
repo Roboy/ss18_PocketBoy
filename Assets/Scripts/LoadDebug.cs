0ac030d37e6b768d7b9d96cc1fd52ac44f5a8490
Egor <egor@defuq.org>
2018-08-13 17:09:30 +0200

    history scene tv and time slider

---
+++ Assets/Scripts/LoadDebug.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadDebug : MonoBehaviour {
+
+    public Button b;
+    public string Scene;
+	// Use this for initialization
+	void Start () {
+		b.onClick.AddListener(() => SceneManager.LoadScene(Scene));
+	}
+}
---
+++ Assets/Scripts/MathUtility.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MathUtility {
+
+    /// <summary>
+    /// Wraps the given index from 0 to arrayLength.
+    /// In Unity "%" is the mathematical rem not mod, they behave differentely for different signs of a,b so we need to transform rem to mod.
+    /// Source: http://answers.unity.com/answers/1120641/view.html
+    /// </summary>
+    /// <param name="index"></param>
+    /// <param name="arrayLength"></param>
+    /// <returns></returns>
+    public static int WrapArrayIndex(int index, int arrayLength)
+    {
+        return (index % arrayLength + arrayLength) % arrayLength;
+    }
+}
