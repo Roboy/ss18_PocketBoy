530b38df4af3c41fba36a13ddf7d2270d102112b
soapinduty <g4mrator@googlemail.com>
2018-10-10 19:37:37 +0200

    Swapping cups now possible, cups on the side have a circular movement.

---
+++ Assets/ShuffleMaster.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pocketboy.Common;
+
+namespace Pocketboy.Cupgame
+{
+    public class ShuffleMaster : Singleton<ShuffleMaster>
+    {
+
+        public GameObject[] Cups;
+
+
+    }
+}
--- Assets/Topics/Experimental-InProgress/CupGame/Scripts/DragMe.cs
+++ Assets/Topics/Experimental-InProgress/CupGame/Scripts/DragMe.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
@@ -8,6 +9,10 @@ namespace Pocketboy.Cupgame
 
     public class DragMe : MonoBehaviour, IPointerUpHandler, IPointerDownHandler
     {
+        public enum RotationDirection { left, right, none };
+        public enum RotationLane { front, back, none };
+
+
         public GameObject PlayingField;
 
         private bool m_IsTouched;
@@ -20,7 +25,9 @@ namespace Pocketboy.Cupgame
 
         private bool m_KinematicState;
 
-        private Vector3 m_OriginalPosition;
+        private Vector3 m_OriginalPosition { get; set; }
+        private bool m_Lifted;
+
 
         /// <summary>
         /// Offset when on touch down between the touch position and the position to avoid a snap to the center.
@@ -42,6 +49,7 @@ namespace Pocketboy.Cupgame
             }
 
             m_OriginalPosition = transform.position;
+            m_Lifted = false;
         }
 
         public void OnPointerDown(PointerEventData eventData)
@@ -74,8 +82,7 @@ namespace Pocketboy.Cupgame
 
             m_DistanceToCameraOnTouch = (Camera.main.transform.position - transform.position).magnitude;
             m_OffsetOnTouch = (Camera.main.ScreenToWorldPoint(new Vector3(eventData.position.x, eventData.position.y, m_DistanceToCameraOnTouch)) - transform.position);
-            gameObject.GetComponent<BoxCollider>().isTrigger = true;
-            
+
         }
 
         void StopDrag()
@@ -89,57 +96,246 @@ namespace Pocketboy.Cupgame
                 m_RigidBody.isKinematic = m_KinematicState;
             }
 
-            gameObject.GetComponent<BoxCollider>().isTrigger = false;
+            StartCoroutine(SwapCups());
+
+
         }
 
-        void UpdateDrag()
+        private void UpdateDrag()
         {
+            //See if the cup needs to be manipulated in the y-axis.
+            StartCoroutine(CheckForElevation());
+
 #if UNITY_EDITOR
             m_TouchPositionScreenSpace = Input.mousePosition;
 #elif UNITY_ANDROID
             m_TouchPositionScreenSpace = Input.touches[0].position;
 #endif
             m_TouchPositionWorldSpace = m_TouchPositionScreenSpace;
-            m_TouchPositionWorldSpace.z = m_DistanceToCameraOnTouch; // transform touch position from 2d to 3d on the plane where the first touch occured
+            m_TouchPositionWorldSpace.z = m_DistanceToCameraOnTouch; // transform touch position from 2d to 3d on the plane where the first touch occured.
             Vector3 NextPosition = Camera.main.ScreenToWorldPoint(m_TouchPositionWorldSpace) - m_OffsetOnTouch;
             transform.position = new Vector3(NextPosition.x, transform.position.y, transform.position.z);
-
-            //CheckForElevation();
         }
 
-        private void OnTriggerEnter(Collider other)
+        private IEnumerator CheckForElevation()
         {
-            //float newHeight = Mathf.Abs(other.gameObject.transform.position.y);
-           
-            //transform.position = new Vector3(transform.position.x, transform.position.y + newHeight, transform.position.z);
-        }
 
-        private void CheckForElevation()
-        {
+
             gameObject.layer = LayerMask.NameToLayer("Ignore");
             int layerMask = ~(1 << LayerMask.NameToLayer("Ignore"));
 
+            //Collision happen.
             if (Physics.OverlapBox(transform.position, Vector3.one, Quaternion.identity, layerMask).Length > 0)
             {
+
                 //If colliding and not elevated, increase the height, so that the cups stack
                 if (transform.position.y == m_OriginalPosition.y)
                 {
-                    transform.position = new Vector3(transform.position.x, transform.position.y + transform.position.y, transform.position.z);
+                    yield return StartCoroutine(ElevateCup(0, gameObject));
                 }
                 else
                 {
                     //keep the current y value, do not change
                 }
             }
-            //If not colliding, reset the y value to the plane surface
-            else
+
+            //No collisions occur.
+            if (Physics.OverlapBox(transform.position, Vector3.one, Quaternion.identity, layerMask).Length == 0)
             {
-                transform.position = new Vector3(m_OriginalPosition.x, m_OriginalPosition.y, transform.position.z);
+
+                if (m_Lifted)
+                {
+                    //If the cup was lifted up, it has to come down
+                    yield return StartCoroutine(ElevateCup(1, gameObject));
+                }
+
             }
 
             gameObject.layer = LayerMask.NameToLayer("Default");
+            yield return null;
         }
 
+        private IEnumerator ElevateCup(int mode, GameObject cup)
+        {
+
+            float startingHeight = cup.transform.position.y;
+            Vector3 currentPos = cup.transform.position;
+            float offset = gameObject.GetComponent<Renderer>().bounds.size.y;
+            offset += (0.1f * offset);
+
+            float endHeight = 0.0f;
+            float currentDuration = 0.0f;
+            float duration = 0.05f;
+
+            //Increase of height.
+            if (mode == 0)
+            {
+                m_Lifted = true;
+                endHeight = startingHeight + offset;
+
+            }
+
+            //Decrease of height.
+            if (mode == 1)
+            {
+                m_Lifted = false;
+                endHeight = startingHeight - offset;
+            }
+
+            //Move the cup over time.
+            while (currentDuration < duration)
+            {
+                currentPos = cup.transform.position;
+                currentPos.y = Mathf.Lerp(startingHeight, endHeight, currentDuration / duration);
+                cup.transform.position = currentPos;
+                currentDuration += Time.deltaTime;
+                yield return null;
+            }
+
+            //Eliminate accuracy error.
+            cup.transform.position = new Vector3(cup.transform.position.x, endHeight, cup.transform.position.z);
+            yield return null;
+        }
+
+
+        private IEnumerator SwapCups()
+        {
+            gameObject.layer = LayerMask.NameToLayer("Ignore");
+            int layerMask = ~(1 << LayerMask.NameToLayer("Ignore"));
+            Collider[] hitted_Objects = Physics.OverlapBox(transform.position, Vector3.one, Quaternion.identity, layerMask);
+
+            if (hitted_Objects.Length == 0)
+            {
+                //no swapping needed.
+            }
+            if (hitted_Objects.Length > 0)
+            {
+                RotationDirection dir = RotationDirection.none;
+                RotationLane lan = RotationLane.none;
+                //swapping needed
+                if (m_Lifted)
+                {
+
+                    int index_a = Array.IndexOf(ShuffleMaster.Instance.Cups, gameObject);
+                    int index_b = Array.IndexOf(ShuffleMaster.Instance.Cups, hitted_Objects[0].gameObject);
+                    int distance = Mathf.Abs(index_a - index_b);
+                    if (index_a < index_b)
+                    {
+                        dir = RotationDirection.left;
+                        lan = RotationLane.front;
+                    }
+                    if (index_b < index_a)
+                    {
+                        dir = RotationDirection.right;
+                        lan = RotationLane.back;
+                    }
+
+                    //Cups are near to each other
+                    if (distance == 1)
+                    {
+                        yield return StartCoroutine(ShuffleMaster.Instance.Cups[index_b].gameObject.GetComponent<DragMe>().MoveCupLinear(m_OriginalPosition, 0.5f));
+                        yield return StartCoroutine(MoveCupLinear(ShuffleMaster.Instance.Cups[index_b].GetComponent<DragMe>().m_OriginalPosition, 0.5f));
+                        GameObject other_cup = ShuffleMaster.Instance.Cups[index_b];
+                        ShuffleMaster.Instance.Cups[index_b] = gameObject;
+                        ShuffleMaster.Instance.Cups[index_a] = other_cup;
+                        gameObject.GetComponent<DragMe>().resetOriginalPosition();
+                        other_cup.GetComponent<DragMe>().resetOriginalPosition();
+
+                    }
+                    //Cups are far to each other
+                    if (distance == 2)
+                    {
+                        yield return StartCoroutine(ShuffleMaster.Instance.Cups[index_b].gameObject.GetComponent<DragMe>().MoveCupCircular(m_OriginalPosition, 0.5f, dir, lan));
+                        yield return StartCoroutine(MoveCupLinear(ShuffleMaster.Instance.Cups[index_b].GetComponent<DragMe>().m_OriginalPosition, 0.5f));
+                        GameObject other_cup = ShuffleMaster.Instance.Cups[index_b];
+                        ShuffleMaster.Instance.Cups[index_b] = gameObject;
+                        ShuffleMaster.Instance.Cups[index_a] = other_cup;
+                        gameObject.GetComponent<DragMe>().resetOriginalPosition();
+                        other_cup.GetComponent<DragMe>().resetOriginalPosition();
+
+                    }
+
+                    //determine offsets
+                    //code here
+
+
+                }
+            }
+            m_Lifted = false;
+            gameObject.layer = LayerMask.NameToLayer("Default");
+
+            yield return null;
+        }
+
+        public IEnumerator MoveCupLinear(Vector3 destination, float duration)
+        {
+            float currentDuration = 0.0f;
+            Vector3 start = gameObject.transform.position;
+
+            while (currentDuration < duration)
+            {
+                gameObject.transform.position = Vector3.Lerp(start, destination, currentDuration / duration);
+                currentDuration += Time.deltaTime;
+                yield return null;
+            }
+
+            gameObject.transform.position = destination;
+
+
+
+        }
+
+        public IEnumerator MoveCupCircular(Vector3 destination, float duration, RotationDirection dir, RotationLane lan)
+        {
+            float currentDuration = 0.0f;
+            float currentAngle = 0.0f;
+            float startingAngle = 0.0f;
+            float resultingAngle = 0.0f;
+
+            if (dir == RotationDirection.left)
+            {
+                startingAngle = 90.0f * Mathf.Deg2Rad;
+                resultingAngle = 270.0f * Mathf.Deg2Rad;
+
+            }
+            if (dir == RotationDirection.right)
+            {
+                startingAngle = 270.0f * Mathf.Deg2Rad;
+                resultingAngle = 90.0f * Mathf.Deg2Rad;
+            }
+
+            float distance = Vector3.Distance(ShuffleMaster.Instance.Cups[1].transform.position, transform.position);
+            Vector3 start = gameObject.transform.position;
+            Vector3 pivotPoint = ShuffleMaster.Instance.Cups[1].transform.position;
+
+
+            while (currentDuration < duration)
+            {
+                currentAngle = Mathf.Lerp(startingAngle, resultingAngle, currentDuration / duration);
+                Vector3 nextPos = gameObject.transform.position;
+                nextPos.x = pivotPoint.x + Mathf.Sin(currentAngle) * distance;
+                if (lan == RotationLane.front)
+                {
+                    nextPos.z = pivotPoint.x + Mathf.Cos(currentAngle) * distance;
+                }
+                if (lan == RotationLane.back)
+                {
+                    nextPos.z = pivotPoint.x - Mathf.Cos(currentAngle) * distance;
+                }
+                gameObject.transform.position = nextPos;
+                currentDuration += Time.deltaTime;
+                yield return null;
+            }
+
+            gameObject.transform.position = destination;
+
+
+        }
+
+        public void resetOriginalPosition()
+        {
+            m_OriginalPosition = gameObject.transform.position;
+        }
     }
 }
 
