ca374d825eb64c71377e551cfed1490c62b5beb4
soapinduty <g4mrator@googlemail.com>
2018-10-15 15:33:12 +0200

    GlassBreaker supports now shattering glass if the volume of the users input is high enough.

---
+++ Assets/Topics/Experimental-InProgress/GlassBreaker/Scripts/Break.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Break : MonoBehaviour
+{
+    public Transform brokenObject;
+    public float magnitudeCol, radius, power, upwards;
+
+    private bool m_ScreenBroken = false;
+
+    //void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.relativeVelocity.magnitude > magnitudeCol)
+    //    {
+    //        Destroy(gameObject);
+    //        Instantiate(brokenObject, transform.position, transform.rotation);
+    //        brokenObject.localScale = transform.localScale;
+    //        Vector3 explosionPos = transform.position;
+    //        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+
+    //        foreach (Collider hit in colliders)
+    //        {
+    //            if (hit.GetComponent<Rigidbody>())
+    //            {
+
+    //                hit.GetComponent<Rigidbody>().AddExplosionForce(power * collision.relativeVelocity.magnitude, explosionPos, radius, upwards);
+    //            }
+    //        }
+    //    }
+    //}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (!m_ScreenBroken)
+            {
+                BreakScreen();
+            }
+        }
+    }
+
+    public void BreakScreen()
+    {
+
+        Destroy(gameObject);
+        Instantiate(brokenObject, transform.position, transform.rotation);
+        brokenObject.localScale = transform.localScale;
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, radius);
+
+        foreach (Collider hit in colliders)
+        {
+            if (hit.GetComponent<Rigidbody>())
+            {
+                hit.GetComponent<Rigidbody>().AddExplosionForce(power, explosionPos, radius, upwards);
+                //hit.GetComponent<Rigidbody>().AddExplosionForce(power*collision.relativeVelocity.magnitude, explosionPos, radius, upwards);
+            }
+        }
+
+
+    }
+}
