1c6069e0d21be244bfe8deff44474258ab0f7120
Egor <egor@defuq.org>
2018-12-29 16:17:49 +0100

    presi

--- Assets/Modules/Common/Scripts/LEDArray.cs
+++ Assets/Modules/Common/Scripts/LEDArray.cs
@@ -15,20 +15,75 @@ namespace Pocketboy.Common
         [SerializeField]
         private Color HighlightColor = Color.white;
 
+        [SerializeField]
+        private Material LEDMaterial;
+
+        Gradient gradient = new Gradient();
+        float currentDuration = 0f;
+        float duration = 5f;
+
         private void Start()
         {
-            StartCoroutine(DefaultAnimation());
+            var colorKey = new GradientColorKey[3];
+            colorKey[0].color = Color.red;
+            colorKey[0].time = 0f;
+            colorKey[1].color = Color.green;
+            colorKey[1].time = 0.5f;
+            colorKey[2].color = Color.blue;
+            colorKey[2].time = 1f;
+
+            var alphaKey = new GradientAlphaKey[3];
+            alphaKey[0].alpha = alphaKey[1].alpha = alphaKey[2].alpha = 1f;
+            alphaKey[0].time = 0f;
+            alphaKey[1].time = 0.5f;
+            alphaKey[2].time = 1f;
+
+            gradient.SetKeys(colorKey, alphaKey);
+        }
+
+        private void Update()
+        {
+            if (currentDuration > duration)
+            {
+                currentDuration = 0f;
+            }
+            LEDMaterial.color = gradient.Evaluate(currentDuration / duration);
+            currentDuration += Time.deltaTime;
+
+
         }
 
         IEnumerator DefaultAnimation()
         {
+            Gradient gradient = new Gradient();
+            var colorKey = new GradientColorKey[3];
+            colorKey[0].color = Color.red;
+            colorKey[0].time = 0f;
+            colorKey[1].color = Color.green;
+            colorKey[1].time = 0.5f;
+            colorKey[2].color = Color.blue;
+            colorKey[2].time = 1f;
+
+            var alphaKey = new GradientAlphaKey[3];
+            alphaKey[0].alpha = alphaKey[1].alpha = alphaKey[2].alpha = 1f;
+            alphaKey[0].time = 0f;
+            alphaKey[1].time = 0.5f;
+            alphaKey[2].time = 1f;
+
+            gradient.SetKeys(colorKey, alphaKey);
             int currentLEDIndex = 0;
+            float duration = 5f;
+            float currentDuration = 0f;
             while (true)
             {
-                LEDs[currentLEDIndex].sharedMaterial.SetColor("_EmissionColor", Color.black);
-                currentLEDIndex = MathUtility.WrapArrayIndex(currentLEDIndex + 1, LEDs.Count);
-                LEDs[currentLEDIndex].sharedMaterial.SetColor("_EmissionColor", HighlightColor);
-                yield return new WaitForSeconds(HighlightTimePerLED);
+                if (currentDuration > duration)
+                {
+                    currentDuration = 0f;
+                }
+                LEDMaterial.color = gradient.Evaluate(currentDuration / duration);
+                currentDuration += Time.deltaTime;
+
+                
             }
         }
     }
